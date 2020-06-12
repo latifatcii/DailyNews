7d7f100646c51241151ead1a23dba49228a5a734
Abdullatif Atci <latif.800@gmail.com>
2020-06-12 10:57:31 +0200

    Add Categories tests

---
+++ DailyNewsUnitTests/ViewModelTests/SearchNewsViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  SearchNewsViewModelTests.swift
+//  DailyNewsUnitTests
+//
+//  Created by Latif Atci on 6/12/20.
+//  Copyright © 2020 Latif Atci. All rights reserved.
+//
+
+import XCTest
+@testable import DailyNews
+
+class SearchNewsViewModelTests: XCTestCase {
+
+    
+
+}
